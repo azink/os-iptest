23b0d70fbbfecb0b3ed1b8444a232198731ddafd
Aaron Zink <azink@solsar.com>
2014-11-17 16:08:02 -0800

    Adding stupid usefulness!

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<?php
-	print($_SERVER['REMOTE_ADDR']);
-?>
+<h1>Warning!</h1>
+<h2>Your cpmputer is broadcasting an Internet Protocol Address!!!</h2>
+<p>We know you are accessing this very high-tech application from: <?php= $_SERVER['REMOTE_ADDR'] ?></p>
+<hr>
+<i>Deployed from 38,000' with OpenShift</i>
